d37d145006d2ace2a301f58b2a4d9dd9dc37ecc7
Alex Perry <alex.perry422@gmail.com>
2025-03-05 18:54:38 +0000

    basic animation scroll

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,7 +1,10 @@
 'use client';
 import React, { use, useEffect, useState } from 'react';
-import Image from 'next/image';
 import { DiscogsRelease } from '@/types/release';
+// import AlbumDisplay from '@/components/album-display';
+import Image from 'next/image';
+import Link from 'next/link';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 
 interface apiResponse {
   pagination: {
@@ -24,6 +27,7 @@ export default function UsernameDisplayPage({
 }) {
   const { username } = use(params);
   const [data, setData] = useState<apiResponse | null>(null);
+  const [testArr, setTestArr] = useState<DiscogsRelease[]>([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -32,6 +36,8 @@ export default function UsernameDisplayPage({
       });
       const json: apiResponse = await res.json();
       setData(json);
+      // set testArr to the first 10 releases
+      setTestArr(json.releases.slice(0, 5));
     }
     fetchData();
   }, [username]);
@@ -42,20 +48,29 @@ export default function UsernameDisplayPage({
         {username}’s Collection
       </h1>
       {data && (
-        <div>
-          {/* TODO: add pagination */}
-          {/* <pre>{JSON.stringify(data.pagination, null, 2)}</pre> */}
-          <div className='grid md:grid-cols-2 xl:grid-cols-3 gap-4'>
-            {data.releases.map((release) => (
-              <div key={release.release_id} className='flex gap-4'>
-                <Image
-                  src={release.thumb}
-                  alt={release.title}
-                  width={144}
-                  height={144}
-                  className='w-36 h-36'
-                />
-                <div className='flex flex-col justify-center'>
+        <div
+          className="relative mx-20 overflow-hidden flex flex-nowrap group-slide py-12 
+          before:absolute before:top-0 before:left-0 before:w-26 before:h-full before:content-[''] before:z-[2] before:bg-gradient-to-l before:from-transparent before:to-background 
+          after:absolute after:top-0 after:right-0 after:w-26 after:h-full after:content-[''] after:z-[2] after:bg-gradient-to-r after:from-transparent after:to-background"
+        >
+          <div className='flex pl-16 gap-16 animate-slide'>
+            {testArr.map((release) => (
+              <Link
+                key={release.release_id}
+                href={`https://discogs.com/release/${release.release_id}`}
+                target='_blank'
+                className='relative group/link w-sm'
+              >
+                <AspectRatio ratio={1}>
+                  <Image
+                    src={release.cover_image}
+                    alt={release.title}
+                    width={576}
+                    height={576}
+                    className='w-full h-full'
+                  />
+                </AspectRatio>
+                <div className='absolute inset-0 flex flex-col justify-center items-center bg-black/0 opacity-0 group-hover/link:opacity-100 group-hover/link:bg-black/60 transition-all duration-200 text-center'>
                   <p className='text-lg font-bold'>{release.title}</p>
                   <p className='text-gray-400'>
                     {release.artists.map((artist, i) => (
@@ -66,7 +81,38 @@ export default function UsernameDisplayPage({
                     ))}
                   </p>
                 </div>
-              </div>
+              </Link>
+            ))}
+          </div>
+          <div className='flex pl-16 gap-16 animate-slide'>
+            {testArr.map((release) => (
+              <Link
+                key={release.release_id}
+                href={`https://discogs.com/release/${release.release_id}`}
+                target='_blank'
+                className='relative group w-sm'
+              >
+                <AspectRatio ratio={1}>
+                  <Image
+                    src={release.cover_image}
+                    alt={release.title}
+                    width={576}
+                    height={576}
+                    className='w-full h-full'
+                  />
+                </AspectRatio>
+                <div className='absolute inset-0 flex flex-col justify-center items-center bg-black/0 opacity-0 group-hover:opacity-100 group-hover:bg-black/50 transition-all duration-200 text-center'>
+                  <p className='text-lg font-bold'>{release.title}</p>
+                  <p className='text-gray-400'>
+                    {release.artists.map((artist, i) => (
+                      <span key={artist}>
+                        {artist}
+                        {i < release.artists.length - 1 && ' · '}
+                      </span>
+                    ))}
+                  </p>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
